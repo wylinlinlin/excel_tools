2d4d5c4f3e2c57ddd94b7e80ff98db14ec4d8b20
kimura <tansen87@qq.com>
2023-02-24 21:22:22 +0800

    Update low_memory.py

--- utils/low_memory.py
+++ utils/low_memory.py
@@ -1,46 +1,61 @@
-import pandas as pd
 import warnings
+from typing import Union
+
+import pandas as pd
+from pandas.core.frame import DataFrame
 
 warnings.filterwarnings("ignore")
 
-class LowMemory:
+
+class LowMemoryUsage:
     def __init__(
-        self, 
-        file_path: str, 
-        sep=",",
-        dtype=None,
-        encoding=None
-        ) -> None:
+            self,
+            file_path: str,  # file path
+            sep=",",  # separator
+            sheet_name: Union[str, None] = None,  # sheet name
+            skip_rows: Union[int, None] = None,  # skip row
+            usecols: Union[str, int, None] = None,  # use column
+            dtype: Union[str, dict[str], None] = None,  # type
+            encoding: Union[str, None] = None  # encoding
+    ) -> None:
         self.file_path = file_path
         self.sep = sep
+        self.sheet_name = sheet_name
+        self.skip_rows = skip_rows
+        self.usecols = usecols
         self.dtype = dtype
-        self.encoding = encoding     
+        self.encoding = encoding
 
-    def read_file(self):
+    def read_file(self) -> DataFrame:
+        """ read file: xls, xlsx, csv, tsv, txt... """
         try:
-            df = pd.read_excel(self.file_path, dtype=self.dtype)
+            df = pd.read_excel(
+                self.file_path, 
+                dtype=self.dtype, 
+                sheet_name=self.sheet_name,
+                skip_rows=self.skip_rows,
+                usecols=self.usecols)
         except:
-            df = pd.read_csv(self.file_path, dtype=self.dtype, encoding=self.encoding, sep=self.sep)
+            df = pd.read_csv(self.file_path, dtype=self.dtype,
+                             encoding=self.encoding, sep=self.sep)
         return df
 
-    def mem_usage(self, pandas_obj: str) -> str:
-        if isinstance(pandas_obj, pd.DataFrame):
-            usage_b = pandas_obj.memory_usage(deep=True).sum()
-        else:  # 假设这不是一个df,而是一个Series
-            usage_b = pandas_obj.memory_usage(deep=True)
-        usage_mb = usage_b / 1024 ** 2  # 将bytes转化成megabytes
-        return "{:03.2f} MB".format(usage_mb)
-
     def convert_dtypes(self) -> tuple:
+        """ convert dtype of datafame """
         self.df = self.read_file()
 
-        df_int = self.df.select_dtypes(include=['int'])  # 用DataFrame.select_dtypes来选中表中的int数据
-        df_int = df_int.apply(pd.to_numeric, downcast='unsigned')  # 用 pd.to_numeric()来降低我们的数据类型
+        # select int type in dataframe
+        df_int = self.df.select_dtypes(include=['int'])
+        # reduce data type
+        df_int = df_int.apply(pd.to_numeric, downcast='unsigned')
 
-        df_float = self.df.select_dtypes(include=['float'])  # 用DataFrame.select_dtypes来选中表中的float数据
-        df_float = df_float.apply(pd.to_numeric, downcast='float')  # 用pd.to_numeric()来降低我们的数据类型
+        # select float type in dataframe
+        df_float = self.df.select_dtypes(include=['float'])
+        # reduce data type
+        df_float = df_float.apply(pd.to_numeric, downcast='float')
 
-        df_obj = self.df.select_dtypes(include=['object'])   # 用DataFrame.select_dtypes来选中表中的object数据
+        # select object type in datafame
+        df_obj = self.df.select_dtypes(include=['object'])
         for col in df_obj.columns:
             num_unique_values = len(df_obj[col].unique())
             num_total_values = len(df_obj[col])
@@ -48,12 +63,12 @@ class LowMemory:
                 df_obj.loc[:, col] = df_obj[col].astype('category')
             else:
                 df_obj.loc[:, col] = df_obj[col]
-        print(f"dtypes converted.")
         return df_int, df_float, df_obj
-    
-    def converted_df(self):
+
+    def converted_df(self) -> DataFrame:
         tup_dtypes = self.convert_dtypes()
         self.df[tup_dtypes[0].columns] = tup_dtypes[0]
         self.df[tup_dtypes[1].columns] = tup_dtypes[1]
         self.df[tup_dtypes[2].columns] = tup_dtypes[2]
+        print(f"dtypes converted...")
         return self.df
